031ff37f4882a5e4aff5862c5b086d7942b59fef
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-04-29 11:01:23 +0300

    переробив 1 задачу

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,18 @@
-const category = document.querySelector('ul#categories');
+const category = document.querySelector("ul#categories");
 const numberOfCategoty = category.children.length;
 console.log(`В списке ${numberOfCategoty} категории.`);
 
+const categoryElem = document.querySelectorAll(".item");
+categoryElem.forEach((element) => {
+  const nameOfElements = element.querySelector("h2").textContent;
+  console.log(`Категория: ${nameOfElements}`);
 
-const categoryElem = document.querySelectorAll('.item');
-const nameOfElements = categoryElem[0].firstElementChild.textContent;
-console.log(`Категория: ${nameOfElements}`);
+  const numberOfElements = element.querySelectorAll(".item li");
+  console.log(`Количество элементов: ${numberOfElements.length} `);
+});
 
-const numberOfElements = categoryElem[0].querySelectorAll('li');
-console.log(`Количество элементов: ${numberOfElements.length} `);
+///const nameOfElements = categoryElem[0].firstElementChild.textContent;
+//console.log(`Категория: ${nameOfElements}`);
+
+//const numberOfElements = categoryElem[0].querySelectorAll('li');
+//console.log(`Количество элементов: ${numberOfElements.length} `);
