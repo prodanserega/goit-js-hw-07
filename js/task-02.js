b74ad676a5e839fdb7466e0197b65f4c71e1beae
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-04-29 11:12:16 +0300

    переробив 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,22 +1,25 @@
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
-const listIngredients = document.querySelector('ul#ingredients');
+const listIngredients = document.querySelector("ul#ingredients");
 
+const elementsLi = ingredients.map(el => {
+  const navItems = document.createElement('li');
+  navItems.textContent = el;
+  return navItems;
+})
+//const elementsLi = [];
 
-
-const elementsLi = [];
-
-for (let i = 0; i < ingredients.length; i += 1) {
-    const textElement = ingredients[i];
-    const navItems = document.createElement('li');
-    navItems.textContent = textElement;
-    elementsLi.push(navItems);
-}
+//for (let i = 0; i < ingredients.length; i += 1) {
+//    const textElement = ingredients[i];
+//    const navItems = document.createElement('li');
+//    navItems.textContent = textElement;
+//   elementsLi.push(navItems);
+///}
 
 listIngredients.append(...elementsLi);
